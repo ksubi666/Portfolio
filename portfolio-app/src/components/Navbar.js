748c4095f98c8bae4606a7e74d65c32b1d2bf855
ksubi666 <ksubi666@gmail.com>
2024-06-26 16:39:48 +0800

    .

--- portfolio-app/src/components/Navbar.js
+++ portfolio-app/src/components/Navbar.js
@@ -7,7 +7,7 @@ const changeTheme = () => {
 
 export const Navbar = ({ handlerClick }) => {
   return (
-    <div className=" dark:bg-[#030712] items-center md:py-4 md:px-28 md:items-center md:font-medium flex justify-between p-4 max-w-[1280px]">
+    <div className=" dark:bg-[#030712] items-center md:py-4 md:px-28 md:items-center md:font-medium flex justify-between p-4">
       <p className="text-gray-900 text-3xl font-bold dark:text-[#F9FAFB]">
         {s}
       </p>
