245a7445797b3a1d0928da1749aa6c02ed6fd1c8
ksubi666 <ksubi666@gmail.com>
2024-06-27 14:34:06 +0800

    .

--- portfolio-app/src/components/Card.js
+++ portfolio-app/src/components/Card.js
@@ -32,7 +32,7 @@ const Card = () => {
   ];
   return CardItems.map((el) => {
     return (
-      <div className="dark:bg-[#374151] w-[343px] md:w-[896px] h-[348px] md:h-[fit-content] p-8 rounded-xl bg-white mb-6 shadow-lg flex flex-col gap-y-4 md:flex-row md:gap-x-12">
+      <div className="dark:bg-[#374151] min-w-[343px] md:max-w-[896px] h-[fit-content] p-8 rounded-xl bg-white mb-6 shadow-lg flex flex-col gap-y-4 md:flex-row md:gap-x-12">
         <img className="w-[102px] h-7 " src={el.icon} alt="upwork"></img>
         <div className=" md:flex-row-reverse md:flex md:gap-x-12">
           <p className="dark:text-[#D1D5DB] text-base text-gray-700 mb-4">
