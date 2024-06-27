6ffae38aef7e99306356432a7cd9f84a95bd06c3
ksubi666 <ksubi666@gmail.com>
2024-06-27 12:06:50 +0800

    .

--- portfolio-app/src/components/WorkCard.js
+++ portfolio-app/src/components/WorkCard.js
@@ -15,7 +15,7 @@ const buttons = [
 ];
 const WorkCard = () => {
   return (
-    <div className="flex flex-col items-center md:flex md:flex-row md:justify-center md:mb-12 mb-6 md:even:flex-row-reverse">
+    <div className="flex flex-col items-center md:flex md:flex-row md:justify-center md:mb-12 mb-6 [&:nth-child(even)]:flex-row-reverse ">
       <div className="dark:bg-[#374151] bg-gray-100 p-8 rounded-t-lg w-[343px] h-[256px] md:min-h-[480px] md:min-w-[576px] md:rounded-none md:rounded-l-lg shadow-lg ">
         <img
           className="size-full rounded-lg"
@@ -23,7 +23,7 @@ const WorkCard = () => {
           alt="WorkCardImg"
         ></img>
       </div>
-      <div className="dark:bg-[#1F2937] p-8 md:p-8 w-[343px] md:h-[480px] md:w-[576px] shadow-lg rounded-b-lg md:rounded-none md:rounded-r-lg ">
+      <div className="dark:bg-[#1F2937] p-8 md:p-8 w-[343px] md:h-[480px] md:w-[576px] shadow-lg rounded-b-lg md:rounded-none md:rounded-r-lg">
         <h1 className="pt-8 dark:text-[#F9FAFB] text-gray-900 text-lg font-bold mb-6">
           Fiskil
         </h1>
